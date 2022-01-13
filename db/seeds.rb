7274cb87ea659c03f1ca26421fc85853a87fbfbd
ben_astengo <astengo93@gmail.com>
2022-01-12 19:04:48 -0600

    Adds w02 day3 homework

---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+# actor.save
+
+# actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
+
+actor = Actor.new(first_name: FFaker::Name.unique.first_name , last_name: FFaker::Name.unique.last_name, known_for: FFaker::Movie.title)
+
+actor.save
