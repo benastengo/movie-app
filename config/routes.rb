82a78db99b5567d8fd75054a4c1bde593f96195d
ben_astengo <astengo93@gmail.com>
2022-01-27 22:39:03 -0600

    Adds authentications and authorizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
     get "/movies" => "movies#index"
-
     post "/movies" => "movies#create"
-
     get "/movies/:id" => "movies#show"
-
     patch "/movies/:id" => "movies#update"
-
     delete "/movies/:id" => "movies#destroy"
 
   
     get "/actors" => "actors#index"
-
     post "/actors" => "actors#create"
-
     get "/actors/:id" => "actors#show"
-
     patch "/actors/:id" => "actors#update"
-
     delete "/actors/:id" => "actors#destroy"
+
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
     
   end
 
